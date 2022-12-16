b27bec2458fc2e157b6e436b2807dd5eeb0dcb14
alexsilva <geniofuturo@gmail.com>
2022-12-16 19:03:46 -0300

    implementado o modelo 'Negotiation'.

--- irpf/models.py
+++ irpf/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Negotiation(models.Model):
+	"""Data do Negócio / Tipo de Movimentação / Mercado / Prazo/Vencimento / Instituição /
+	Código de Negociação / Quantidade / Preço / Valor"""
+
+	date = models.DateField(verbose_name="Data do Negócio")
+	kind = models.CharField(verbose_name="Tipo de Movimentação",
+	                        max_length=16)
+
+	institution = models.CharField(verbose_name="Instituição")
+
+	code = models.CharField(verbose_name="Código de negociação",
+	                        max_length=8)
+
+	quantity = models.PositiveBigIntegerField(verbose_name="Quantidade")
+
+	price = models.FloatField(verbose_name="Preço", default=0.0)
+
+	total = models.FloatField(verbose_name="Valor (total)", default=0.0)
+
+	# relates the name of the headers with the fields.
+	date.sheet_header = "Data do Negócio"
+	kind.sheet_header = "Tipo de Movimentação"
+	institution.sheet_header = "Instituição"
+	code.sheet_header = "Código de Negociação"
+	quantity.sheet_header = "Quantidade"
+	price.sheet_header = "Preço"
+	total.sheet_header = "Valor"
+
+	def __str__(self):
+		return f'{self.code}/{self.quantity} ({self.institution})'
+
+	class Meta:
+		verbose_name = "Negotiation"
