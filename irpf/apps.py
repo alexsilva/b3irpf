55877e0926510b878220a38f1993cfd6a9d13c2f
alexsilva <geniofuturo@gmail.com>
2022-12-20 20:56:34 -0300

    Alterando o título do app.

--- irpf/apps.py
+++ irpf/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class IrpfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
+    verbose_name = 'Declaração do imposto anual'
     name = 'irpf'
