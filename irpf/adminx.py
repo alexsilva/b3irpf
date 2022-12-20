9cb04ce186dd9e9b7b70512cb1ff135e9aefb247
alexsilva <geniofuturo@gmail.com>
2022-12-20 17:06:04 -0300

    preparação para multiplos comandos.

--- irpf/adminx.py
+++ irpf/adminx.py
@@ -1,10 +1,10 @@
 from irpf.models import Enterprise, Negotiation
-from irpf.plugins import ImportListModelPlugin
+from irpf.plugins import ListActionModelPlugin
 from irpf.views import AdminImportListModelView
 from xadmin import sites, site
 from xadmin.views import ListAdminView
 
-site.register_plugin(ImportListModelPlugin, ListAdminView)
+site.register_plugin(ListActionModelPlugin, ListAdminView)
 site.register_view("^irpf/import_listmodel/(?P<model_app_label>.+)/$", AdminImportListModelView,
                    "import_listmodel")
 
--- irpf/plugins.py
+++ irpf/plugins.py
@@ -1,27 +1,33 @@
 import django.forms as django_forms
 from django.core.management import get_commands
+from django.template.loader import render_to_string
 
 from xadmin.plugins.utils import get_context_dict
 from xadmin.views import BaseAdminPlugin
 
 
-class ImportListModelPlugin(BaseAdminPlugin):
+class ListActionModelPlugin(BaseAdminPlugin):
 
 	def init_request(self, *args, **kwargs):
 		self.command_name = f"import_{self.opts.model_name.lower()}"
 		return self.command_name in get_commands()
 
-	def block_top_toolbar(self, context, nodes):
-		context = get_context_dict(context)
+	def get_import_action(self):
 		model_name = f"{self.opts.app_label}.{self.opts.model_name}"
 		url = self.get_admin_url("import_listmodel", model_name)
-		return f"""
-		<div class="btn-group layout-btns ml-2">
-          <a href="{url}" class="btn-link btn-sm import-list">
-            Importa lista de dados
-          </a>
-        </div>
-		"""
+		return {
+			'title': "Importa lista de dados",
+			'url': url
+		}
+
+	def block_top_toolbar(self, context, nodes):
+		context = get_context_dict(context)
+		list_actions_group = {
+			"import": self.get_import_action()
+		}
+		context['list_actions_group'] = list_actions_group
+		return render_to_string("irpf/adminx.block.listtoolbar_action.html",
+		                        context=context)
 
 	def get_media(self, media):
 		media += django_forms.Media(js=[
