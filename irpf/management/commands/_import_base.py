1c1fd93e636d43d940ee5cc710f0488871548e23
alexsilva <geniofuturo@gmail.com>
2022-12-26 21:28:31 -0300

    Importação de dados com vínculo a um usuário.

--- irpf/management/commands/_import_base.py
+++ irpf/management/commands/_import_base.py
@@ -1,17 +1,42 @@
 import argparse
 
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
+from guardian.shortcuts import assign_perm
 from openpyxl import load_workbook
+
+User = get_user_model()
+
+
+class UserType:
+	def __init__(self, queryset):
+		self.queryset = queryset
+		self.model = queryset.model
+		self.opts = self.model._meta
+
+	def __call__(self, value):
+		try:
+			pk = self.opts.pk.to_python(value)
+		except ValidationError:
+			raise argparse.ArgumentTypeError(f"enter a valid primary key.")
+		try:
+			return self.queryset.get(pk=pk)
+		except self.model.DoesNotExist:
+			raise argparse.ArgumentTypeError(f"user not found!")
 
 
 class Command(BaseCommand):
 	help = """imports data from the xlsx file with information on the earnings."""
+	model_permissions = ["view", "add", "change", "delete"]
 
 	storage_model = None
 	storage_ops = None
 
 	def add_arguments(self, parser):
 		parser.add_argument("--filepath", type=argparse.FileType('rb'), required=True)
+		parser.add_argument("--user", type=UserType(User.objects.filter(is_active=True)),
+		                    required=True)
 
 	def get_fields_map(self):
 		fields = {}
@@ -20,7 +45,20 @@ class Command(BaseCommand):
 				fields.setdefault(field.sheet_header, []).append(field)
 		return fields
 
-	def process_sheet(self, ws):
+	def _assign_perm(self, instance, user=None):
+		"""Adiciona permissões padrão para a instância x usuário"""
+		opts = instance._meta
+		if user is None:
+			user = instance.user
+		for name in self.model_permissions:
+			assign_perm(f"{name}_{opts.model_name}", user, instance)
+
+	def save_instance(self, **data):
+		instance, created = self.storage_model.objects.get_or_create(**data)
+		if created:
+			self._assign_perm(instance)
+
+	def process_sheet(self, ws, options):
 		print("SHEET ", ws.title)
 
 		rows = ws.iter_rows()
@@ -32,9 +70,10 @@ class Command(BaseCommand):
 		print(" / ".join(headers))
 
 		fields = self.get_fields_map()
-		data = {}
+		user = options['user']
 		for row in rows:
 			cells = []
+			data = {'user': user}
 			for index, cell in enumerate(row):
 				try:
 					field_list = fields[headers[index]]
@@ -48,7 +87,7 @@ class Command(BaseCommand):
 					data[field_name] = cell.value
 				cells.append(str(cell.value))
 			print(" / ".join(cells))
-			self.storage_model.objects.get_or_create(**data)
+			self.save_instance(**data)
 
 	def handle(self, *args, **options):
 		wb = None
@@ -60,7 +99,7 @@ class Command(BaseCommand):
 				)
 
 				for sheet_name in wb.sheetnames:
-					self.process_sheet(wb[sheet_name])
+					self.process_sheet(wb[sheet_name], options)
 			finally:
 				if wb:
 					wb.close()
