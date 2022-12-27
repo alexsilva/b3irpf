e7883a7bb251f5a4ddc6173b230d3b6edd139d8c
alexsilva <geniofuturo@gmail.com>
2022-12-26 21:29:23 -0300

    Filtro da queryset com permissões de objetos.

--- irpf/adminx.py
+++ irpf/adminx.py
@@ -1,9 +1,11 @@
+from guardian.shortcuts import get_objects_for_user
+
 from irpf.models import Enterprise, Negotiation, Earnings, Position, Instituition, Provision
 from irpf.plugins import ListActionModelPlugin
 from irpf.views.import_list import AdminImportListModelView
 from irpf.views.report_irpf import AdminReportIrpfModelView
 from xadmin import sites, site
-from xadmin.views import ListAdminView
+from xadmin.views import ListAdminView, filter_hook
 
 site.register_plugin(ListActionModelPlugin, ListAdminView)
 site.register_view("^irpf/import-listmodel/(?P<model_app_label>.+)/$", AdminImportListModelView,
@@ -20,6 +22,24 @@ class InstituitionAdmin:
 	)
 
 
+class BaseIRPFAdmin:
+	readonly_fields = ("user",)
+
+	@filter_hook
+	def queryset(self):
+		model_perms = self.get_model_perms()
+		model_perms = [f"{name}_{self.opts.model_name}"
+		               for name in model_perms if model_perms[name]]
+		queryset = get_objects_for_user(
+			self.user,
+			model_perms,
+			klass=self.model,
+			any_perm=True,
+			with_superuser=False,
+			accept_global_perms=False)
+		return queryset
+
+
 @sites.register(Enterprise)
 class EnterpriseAdmin:
 	list_filter = ("category",)
@@ -33,12 +53,12 @@ class EnterpriseAdmin:
 
 
 @sites.register(Position)
-class PositionAdmin:
+class PositionAdmin(BaseIRPFAdmin):
 	...
 
 
 @sites.register(Negotiation)
-class NegotiationAdmin:
+class NegotiationAdmin(BaseIRPFAdmin):
 	list_filter = ("kind", "date")
 	search_fields = ("code",)
 	list_display = (
@@ -52,7 +72,7 @@ class NegotiationAdmin:
 
 
 @sites.register(Earnings)
-class EarningsAdmin:
+class EarningsAdmin(BaseIRPFAdmin):
 	list_filter = ("kind", "date")
 	search_fields = ("code",)
 	list_display = (
@@ -68,7 +88,7 @@ class EarningsAdmin:
 
 
 @sites.register(Provision)
-class ProvisionAdmin:
+class ProvisionAdmin(BaseIRPFAdmin):
 	search_fields = ("code", "kind")
 	list_display = (
 		'code',
