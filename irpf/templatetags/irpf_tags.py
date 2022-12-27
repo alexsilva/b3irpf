f86f1e5fd7334e0004d86e5cbd6da7f0c8558516
alexsilva <geniofuturo@gmail.com>
2022-12-26 21:56:03 -0300

    Considerando valores vazios antes de formatar números.

--- irpf/templatetags/irpf_tags.py
+++ irpf/templatetags/irpf_tags.py
@@ -7,4 +7,6 @@ register = template.Library()
 @register.filter
 def moneyformat(value):
 	"""money format"""
+	if value is None or value == "":
+		value = 0.0
 	return number_format(value, decimal_pos=2)
