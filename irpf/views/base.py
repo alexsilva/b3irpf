0bbc2df15a98db7c57f50dfddd0e2c08882a9e85
alexsilva <geniofuturo@gmail.com>
2022-12-31 16:31:30 -0300

    Adição de classe base para criação view form.

---
+++ irpf/views/base.py
@@ -0,0 +1,14 @@
+from django.views.generic import FormView
+
+from xadmin.views.base import CommAdminView
+
+
+class AdminFormView(CommAdminView, FormView):
+	"""Base classe que implementa parte de uma view de formulário"""
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context.update(self.get_context())
+		form_media = context['form'].media
+		context['media'] += form_media
+		return context
