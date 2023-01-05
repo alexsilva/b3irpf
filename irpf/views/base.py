66f32f1acf593aa0c8e7d96d1aa0f3f239503499
alexsilva <geniofuturo@gmail.com>
2023-01-05 19:38:12 -0300

    Atributos para configuração do formulário.

--- irpf/views/base.py
+++ irpf/views/base.py
@@ -6,6 +6,7 @@ from xadmin.views.base import CommAdminView
 class AdminFormView(CommAdminView, FormView):
 	"""Base classe que implementa parte de uma view de formulário"""
 	title = None
+	form_method_post = False
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
@@ -13,4 +14,5 @@ class AdminFormView(CommAdminView, FormView):
 		form_media = context['form'].media
 		context['media'] += form_media
 		context['title'] = self.title
+		context['form_method_post'] = self.form_method_post
 		return context
