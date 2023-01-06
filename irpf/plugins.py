1fa74c1e8b2c6f129a9ed121945375023c23aa9f
alexsilva <geniofuturo@gmail.com>
2023-01-05 21:49:43 -0300

    Salvando posições.

--- irpf/plugins.py
+++ irpf/plugins.py
@@ -1,11 +1,13 @@
 import django.forms as django_forms
 from django.contrib.auth import get_permission_codename
 from django.core.management import get_commands
+from django.db.transaction import atomic
 from django.template.loader import render_to_string
 from django.utils.functional import cached_property
 from guardian.shortcuts import get_objects_for_user, assign_perm
 
-from irpf.models import Negotiation, Earnings, Provision
+from irpf.models import Negotiation, Earnings, Provision, Position
+from xadmin.plugins import auth
 from xadmin.plugins.utils import get_context_dict
 from xadmin.views import BaseAdminPlugin
 
@@ -99,18 +101,49 @@ class ListActionModelPlugin(BaseAdminPlugin):
 
 class SaveReportPositionPlugin(BaseAdminPlugin):
 	"""Salva os dados de posição do relatório"""
+	position_model = Position
+	position_permission = list(auth.ACTION_NAME)
 
 	def form_valid(self, response, form):
-		if self.is_save_position and self.admin_view.report:
-			self.save_position(self.admin_view.report)
+		if self.is_save_position and self.admin_view.report and self.admin_view.results:
+			self.save_position(form.cleaned_data['end'], self.admin_view.results)
 		return response
 
 	def block_form_buttons(self, context, nodes):
 		if self.admin_view.report:
 			return render_to_string("irpf/blocks/blocks.form.buttons.button_save_position.html")
 
-	def save_position(self, report):
-		print('save')
+	@atomic
+	def save_position(self, date, results):
+		for item in results:
+			enterprise = item['enterprise']
+			institution = item['institution']
+
+			item_result = item['results']
+			compra = item_result['compra']
+
+			defaults = {
+				'quantity': compra['quantity'],
+				'avg_price': compra['avg_price'],
+				'total': compra['total'],
+				'date': date
+			}
+			instance, created = self.position_model.objects.get_or_create(
+				defaults=defaults,
+				enterprise=enterprise,
+				institution=institution
+			)
+			if not created:
+				for field_name in defaults:
+					setattr(instance, field_name, defaults[field_name])
+				instance.save()
+
+			# permissões de objeto
+			for name in self.position_permission:
+				if not self.has_model_perm(self.position_model, name, self.user):
+					continue
+				codename = self.get_model_perm(self.position_model, name)
+				assign_perm(codename, self.user, instance)
 
 	@cached_property
 	def is_save_position(self):
