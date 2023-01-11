2c75b107123021192459dc15a9aa704dfce84c8e
alexsilva <geniofuturo@gmail.com>
2023-01-11 15:48:05 -0300

    Criação da guia outros.

--- irpf/templatetags/irpf_tags.py
+++ irpf/templatetags/irpf_tags.py
@@ -8,3 +8,18 @@ register = template.Library()
 def moneyformat(value):
 	"""money format"""
 	return number_format(value, decimal_pos=2)
+
+
+@register.simple_tag
+def exclude_obj_keys(obj, *keys):
+	results = {}
+	for key in obj:
+		if key in keys:
+			continue
+		results[key] = obj[key]
+	return results
+
+
+@register.simple_tag
+def get_obj_val(obj, key):
+	return obj[key]
