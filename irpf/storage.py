bcc01f069a32b971ca3ae655b2a5a28097eaf8e0
alexsilva <geniofuturo@gmail.com>
2023-02-22 14:28:23 -0300

    Criação de file storage.

---
+++ irpf/storage.py
@@ -0,0 +1,13 @@
+from django.core.files.storage import FileSystemStorage
+
+
+class FileSystemOverwriteStorage(FileSystemStorage):
+    """
+    Custom file system storage: Overwrite get_available_name to make Django replace files instead of
+    creating new ones over and over again.
+    https://gist.github.com/fabiomontefuscolo/1584462
+    """
+
+    def get_available_name(self, name, max_length=None):
+        self.delete(name)
+        return super().get_available_name(name, max_length)
