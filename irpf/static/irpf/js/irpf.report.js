368ddc777f61dd8b0e3b063d37381036a8a76209
alexsilva <geniofuturo@gmail.com>
2023-02-26 21:57:51 -0300

    Bloqueio do botão para salvar posição.

--- irpf/static/irpf/js/irpf.report.js
+++ irpf/static/irpf/js/irpf.report.js
@@ -1,4 +1,8 @@
 $(function () {
+    $(".exform #position_locked").click(function () {
+        var is_checked = $(this).is(":checked");
+        $(".exform button[name='position']").prop("disabled", !is_checked);
+    })
     $('.irpfreport [data-toggle="popover"]').popover({
         animation: false
     });
