6602be7b21c9f6880ac84b88c3c7c11994caeff8
alexsilva <geniofuturo@gmail.com>
2023-03-09 12:21:57 -0300

    Evitando multiplos envios do formulário.

--- irpf/static/irpf/js/irpf.report.js
+++ irpf/static/irpf/js/irpf.report.js
@@ -1,8 +1,21 @@
 $(function () {
-    $(".exform #position_locked").click(function () {
+    var $form = $("form.exform.rended");
+    $form.find("#position_locked").click(function () {
         var is_checked = $(this).is(":checked");
-        $(".exform button[name='position']").prop("disabled", !is_checked);
-    })
+        $form.find("button[name='position']").prop("disabled", !is_checked);
+    });
+    $form.submit(function (evt) {
+        var $fm = $(this),
+            $btn = $(evt.originalEvent.submitter);
+        $fm.data('$submitter', $btn);
+        $btn.prop("disabled", true);
+    });
+    $(document).keyup(function (e) {
+        if (e.keyCode === 27 && $form.data('$submitter')) {
+            $form.data('$submitter').prop("disabled", false);
+            $form.data('$submitter', null);
+        }
+    });
     $('.irpfreport [data-toggle="popover"]').popover({
         animation: false
     });
