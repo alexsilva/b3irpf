b0b4646fb666a397c800834f4c8d7e2dc81f67de
alexsilva <geniofuturo@gmail.com>
2023-03-11 14:56:45 -0300

    Correção no envio do 'name' do botão.

--- irpf/static/irpf/js/irpf.report.js
+++ irpf/static/irpf/js/irpf.report.js
@@ -1,14 +1,31 @@
 $(function () {
-    var $form = $("form.exform.rended");
+    var $form = $("form.exform.rended"),
+        form_submit_handler = function ($form, $submit) {
+            var input_hidden_class = $submit.attr("name") + "_submit";
+            $form.find("input." + input_hidden_class).remove();
+            $submit.each(function () {
+                var $el = $(this);
+                $("<input/>")
+                .addClass(input_hidden_class)
+                .attr("type", "hidden")
+                .attr("name", $el.attr("name"))
+                .attr("value", $el.attr("value"))
+                .appendTo($form);
+            });
+        }
+
     $form.find("#position_locked").click(function () {
-        var is_checked = $(this).is(":checked");
-        $form.find("button[name='position']").prop("disabled", !is_checked);
+        var is_checked = $(this).is(":checked"),
+            $btn = $form.find("button[name='position']");
+        $btn.prop("disabled", !is_checked);
+        form_submit_handler($form, $btn);
     });
     $form.submit(function (evt) {
         var $fm = $(this),
             $btn = $(evt.originalEvent.submitter);
         $fm.data('$submitter', $btn);
         $btn.prop("disabled", true);
+        form_submit_handler($fm, $btn);
     });
     $(document).keyup(function (e) {
         if (e.keyCode === 27 && $form.data('$submitter')) {
