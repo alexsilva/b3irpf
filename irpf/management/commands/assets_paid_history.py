bbd038d1baa369589207294e622014df966b3eda
alexsilva <geniofuturo@gmail.com>
2023-05-03 19:33:01 -0300

    Comando para armzenamento de proventos pagos.

---
+++ irpf/management/commands/assets_paid_history.py
@@ -0,0 +1,19 @@
+from django.utils.timezone import now
+
+from assetprice.management.commands import paid_history
+from irpf.models import Enterprise
+
+
+class Command(paid_history.Command):
+	help = "Armazena o histórico de dividendos dos ativos cadastrados"
+
+	def handle(self, *args, **options):
+		interval = 5
+		date_now = now()
+		for instance in Enterprise.objects.all():
+			queryset = self.get_from_history(instance.code, date_now.year - interval, date_now.year)
+			if queryset.count() < interval:
+				options['ticker'] = instance.code
+				super().handle(**options)
+			else:
+				print(f'[CACHE] {instance}', file=self.stdout)
