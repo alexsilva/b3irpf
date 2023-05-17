fe248b833f551ebf858027143c3b658b0e878267
alexsilva <geniofuturo@gmail.com>
2023-05-17 20:39:08 -0300

    Adicionando opção 'nocache'.

--- irpf/management/commands/assets_paid_history.py
+++ irpf/management/commands/assets_paid_history.py
@@ -7,12 +7,16 @@ from irpf.models import Asset
 class Command(paid_history.Command):
 	help = "Armazena o histórico de dividendos dos ativos cadastrados"
 
+	def add_arguments(self, parser):
+		parser.add_argument('-nc', '--nocache', action="store_true")
+
 	def handle(self, *args, **options):
 		interval = 5
 		date_now = now()
+		no_cache = options.get('nocache', False)
 		for instance in Asset.objects.filter(category=Asset.CATEGORY_STOCK):
 			queryset = self.get_from_history(instance.code, date_now.year - interval, date_now.year)
-			if queryset.count() < interval:
+			if no_cache or queryset.count() < interval:
 				options['ticker'] = instance.code
 				super().handle(**options)
 			else:
