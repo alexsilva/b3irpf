421a0fa36a5ab9250b04355d0386fe0744b9817c
alexsilva <geniofuturo@gmail.com>
2023-06-29 19:09:51 -0300

    Correção na criação da tabela com os dados de 'data-items'.

--- irpf/static/irpf/js/irpf.xlsx.viewer.js
+++ irpf/static/irpf/js/irpf.xlsx.viewer.js
@@ -1,13 +1,12 @@
 $(function () {
     // inicializacao das tabelas de dados
-    var $el = $("table.xlsx_viewer:not(.full)");
-    if ($el.length) {
-        $el.addClass("full");
+    $("table.xlsx_viewer:not(.full)").each(function () {
+        var $el = $(this).addClass("full");
         $el.DataTable({
             data: $el.data("items"),
             language: {
-                url: '/static/irpf/datatables-1.13.1/i18n/' + xadmin.language_code +'.json'
+                url: '/static/irpf/datatables-1.13.1/i18n/' + xadmin.language_code + '.json'
             }
         });
-    }
+    })
 });
