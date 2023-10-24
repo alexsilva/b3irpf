bc4b04ad94526d3a53ae07434976f91a2a9f7dac
alexsilva <geniofuturo@gmail.com>
2023-10-24 02:00:10 -0300

    Adição do método abstrato 'compile'

--- irpf/report/base.py
+++ irpf/report/base.py
@@ -26,7 +26,7 @@ class BaseReport:
 
 	@staticmethod
 	def compile(date: datetime.date, reports):
-		return reports[date.month]
+		raise NotImplementedError
 
 	@staticmethod
 	def results_sorted(item):
