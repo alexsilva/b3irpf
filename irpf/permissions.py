5bd71f1c4837d8a9c5551480fb41a4f74dbc1635
alexsilva <geniofuturo@gmail.com>
2023-10-31 20:08:01 -0300

    Script para armazernas permissões.

---
+++ irpf/permissions.py
@@ -0,0 +1,39 @@
+from assetprice.models import AssetEarningHistory
+from irpf.models import (
+	Bookkeeping,
+	FoundsAdministrator,
+	Asset,
+	Institution,
+	Negotiation,
+	Bonus,
+	Earnings,
+	BrokerageNote,
+	AssetEvent,
+	Position,
+	Taxes,
+	Subscription,
+	BonusInfo,
+	SubscriptionInfo,
+	Statistic
+)
+
+permission_all = ('view', 'add', 'change', 'delete')
+
+permission_models = {
+	AssetEarningHistory: permission_all,
+	Bookkeeping: ('view', 'add'),
+	FoundsAdministrator: ('view', 'add'),
+	Asset: ('view', 'add', 'change'),
+	Institution: ('view', 'add', 'change'),
+	Negotiation: permission_all,
+	Bonus: permission_all,
+	BonusInfo: permission_all,
+	Subscription: permission_all,
+	SubscriptionInfo: permission_all,
+	Earnings: permission_all,
+	BrokerageNote: permission_all,
+	AssetEvent: permission_all,
+	Position: permission_all,
+	Taxes: permission_all,
+	Statistic: permission_all,
+}
