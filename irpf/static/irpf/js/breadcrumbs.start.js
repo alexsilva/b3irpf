097d89f39389d5caab25b909d6cfde1390667d09
alexsilva <geniofuturo@gmail.com>
2023-11-05 19:40:34 -0300

    Executando plugin sem esperar o documento ficar pronto.

--- irpf/static/irpf/js/breadcrumbs.start.js
+++ irpf/static/irpf/js/breadcrumbs.start.js
@@ -1,8 +1,8 @@
-$(function () {
+(function () {
     $('#breadcrumb_months > .breadcrumb').asBreadcrumbs({
         namespace: 'breadcrumb',
         toggleIconClass: 'fa fa-caret-down p-2',
         dropdownItemClass: 'dropdown-item',
         hiddenClass: 'd-none',
     });
-})
+})()
