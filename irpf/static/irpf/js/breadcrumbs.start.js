2d80f9ae7244262e71fcd73ed6a4c38e4b6aa84f
alexsilva <geniofuturo@gmail.com>
2023-12-06 21:12:40 -0300

    Estrutura de menu mais compatível com bootstrap 4.

--- irpf/static/irpf/js/breadcrumbs.start.js
+++ irpf/static/irpf/js/breadcrumbs.start.js
@@ -4,5 +4,16 @@
         toggleIconClass: 'fa fa-caret-down p-2',
         dropdownItemClass: 'dropdown-item',
         hiddenClass: 'd-none',
+        dropdownItem: function (classes, label, href) {
+            var template = '<li class="{{classes.dropdownItemDisableClass}}">' +
+                            '<a class="{{classes.dropdownItemClass}}" href="{{href}}">{{label}}</a>' +
+                            '</li>',
+                options = {
+                    classes: classes,
+                    label: label
+                };
+            options.href = href ? href: "#";
+            return $.fn.nunjucks_env.renderString(template, options);
+        },
     });
 })()
