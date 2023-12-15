2c0d663d702258804289b7426973d2ce5766ee15
alexsilva <geniofuturo@gmail.com>
2023-12-15 19:19:37 -0300

    Alteração do label de Taxes.stats.

--- irpf/models.py
+++ irpf/models.py
@@ -747,7 +747,7 @@ class Taxes(BaseIRPFModel):
 	                           help_text="Uma vez marcado, informa que o imposto já foi pago.")
 	created = models.DateTimeField(verbose_name="Data de registro", auto_now_add=True)
 
-	stats = models.ManyToManyField(Statistic, verbose_name="Statísticas", editable=False)
+	stats = models.ManyToManyField(Statistic, verbose_name="Estatísticas", editable=False)
 
 	@classproperty
 	def taxes_choices(cls):
